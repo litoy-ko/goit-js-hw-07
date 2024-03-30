326d49538a4f50768d0fbb2805961afbc6d49594
litoy-ko <litoys.ark2nd@gmail.com>
2024-03-30 22:35:24 +0800

    updating task1 and task2
    finalized hw7

---
+++ js/01-gallery.js
@@ -0,0 +1,29 @@
+import { galleryItems } from "./gallery-items.js";
+// console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+	let li = `
+    <li class="gallery__item">
+        <a class="gallery__link" href="javascript:void(0)">
+        <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.description}"
+        />
+        </a>
+    </li>
+    `;
+	gallery.innerHTML += li;
+});
+
+//Event Delegation
+gallery.addEventListener("click", (e) => {
+	if (e.target.tagName === "IMG") {
+		let bigImage = e.target.getAttribute("data-source");
+		let instance = basicLightbox.create(`<img src="${bigImage}" width="800" height="600"/>`);
+		instance.show();
+	}
+});
